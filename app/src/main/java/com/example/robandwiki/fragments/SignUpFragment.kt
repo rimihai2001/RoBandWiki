9bd0dc132d312a69d03e08e122761ce5718ee683
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-04-05 16:09:51 +0300

    SignIn-SignUp switch buttons done

--- app/src/main/java/com/example/robandwiki/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/robandwiki/fragments/SignUpFragment.kt
@@ -1,21 +1,37 @@
 package com.example.robandwiki.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.findNavController
 import com.example.robandwiki.R
+import com.example.robandwiki.databinding.FragmentSignUpBinding
 
 
 class SignUpFragment : Fragment() {
+
+    private lateinit var binding: FragmentSignUpBinding
+    private lateinit var navController: NavController
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+        binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        navController = findNavController()
+
+        binding.switchToLoginTextView.setOnClickListener {
+            navController.navigate(R.id.action_signUpFragment_to_signInFragment)
+        }
     }
 
 }
