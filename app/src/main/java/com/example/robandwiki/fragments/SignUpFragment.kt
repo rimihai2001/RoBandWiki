9f04155c6909ca70c3b155b58e0f056c7e7a7891
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-04-07 22:09:19 +0300

    Firebase SignUp done

--- app/src/main/java/com/example/robandwiki/fragments/SignUpFragment.kt
+++ app/src/main/java/com/example/robandwiki/fragments/SignUpFragment.kt
@@ -4,34 +4,61 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.findNavController
 import com.example.robandwiki.R
 import com.example.robandwiki.databinding.FragmentSignUpBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SignUpFragment : Fragment() {
 
-    private lateinit var binding: FragmentSignUpBinding
-    private lateinit var navController: NavController
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        binding = FragmentSignUpBinding.inflate(inflater, container, false)
-        return binding.root
-    }
+        val view = inflater.inflate(R.layout.fragment_sign_up, container, false)
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+        auth = FirebaseAuth.getInstance()
 
-        navController = findNavController()
+        val emailEditText = view.findViewById<EditText>(R.id.email_edit_text)
+        val passwordEditText = view.findViewById<EditText>(R.id.password_edit_text)
+        val signUpButton = view.findViewById<Button>(R.id.signup_button)
+        val switchToLoginTextView = view.findViewById<TextView>(R.id.switch_to_login_text_view)
 
-        binding.switchToLoginTextView.setOnClickListener {
+        signUpButton.setOnClickListener {
+            val email = emailEditText.text.toString()
+            val password = passwordEditText.text.toString()
+
+            if (email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(activity, "Please fill out all fields.", Toast.LENGTH_SHORT).show()
+            } else {
+                auth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
+                            Toast.makeText(activity, "Registration successful!", Toast.LENGTH_SHORT).show()
+                            val navController = Navigation.findNavController(requireView())
+                            navController.navigate(R.id.action_signUpFragment_to_homeFragment)
+                        } else {
+                            Toast.makeText(activity, "Registration failed. Please try again later.", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }
+        }
+
+        switchToLoginTextView.setOnClickListener {
+            val navController = Navigation.findNavController(requireView())
             navController.navigate(R.id.action_signUpFragment_to_signInFragment)
         }
-    }
 
+        return view
+    }
 }
