897b2db569ad16438b8631e19e8344f5eb7179b8
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-04-14 23:52:59 +0300

    Firebase Notifications added

--- app/src/main/java/com/example/robandwiki/MainActivity.kt
+++ app/src/main/java/com/example/robandwiki/MainActivity.kt
@@ -6,15 +6,19 @@ import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.robandwiki.databinding.ActivityMainBinding
 import com.example.robandwiki.fragments.*
+import com.google.firebase.analytics.FirebaseAnalytics
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var firebaseAnalytics: FirebaseAnalytics
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
 
         binding.bottomNavigationView.setOnItemSelectedListener { item ->
             when (item.itemId) {
