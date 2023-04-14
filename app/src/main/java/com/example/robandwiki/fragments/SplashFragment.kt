3111d1d016c2f5fd0512860f41a0a29af086c380
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-04-15 02:14:39 +0300

    Object Animator added on SplashFragment

--- app/src/main/java/com/example/robandwiki/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/robandwiki/fragments/SplashFragment.kt
@@ -1,10 +1,12 @@
 package com.example.robandwiki.fragments
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.LinearInterpolator
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
 import androidx.navigation.fragment.findNavController
@@ -15,25 +17,31 @@ import com.example.robandwiki.databinding.FragmentSplashBinding
 
 
 class SplashFragment : Fragment() {
-        private lateinit var binding: FragmentSplashBinding
-        private lateinit var navController: NavController
+    private lateinit var binding: FragmentSplashBinding
+    private lateinit var navController: NavController
 
-        override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
-        ): View? {
-            binding = FragmentSplashBinding.inflate(inflater, container, false)
-            return binding.root
-        }
-
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
-            //(activity as MainActivity).setBottomNavigationVisibility(View.GONE)
-
-            navController = findNavController()
-
-            binding.buttonEnterNow.setOnClickListener {
-                navController.navigate(R.id.action_splashFragment_to_signInFragment)
-            }
-        }
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentSplashBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //(activity as MainActivity).setBottomNavigationVisibility(View.GONE)
+
+        navController = findNavController()
+
+        binding.buttonEnterNow.setOnClickListener {
+            navController.navigate(R.id.action_splashFragment_to_signInFragment)
+        }
+
+        // Rotate the application name text twice
+        val rotateAnimator = ObjectAnimator.ofFloat(binding.textView, View.ROTATION, 0f, 360f)
+        rotateAnimator.duration = 2000
+        rotateAnimator.repeatCount = 0
+        rotateAnimator.start()
+    }
+}
