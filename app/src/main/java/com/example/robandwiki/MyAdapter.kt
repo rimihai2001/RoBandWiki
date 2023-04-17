cb86d6272f0345392f531f267f5202ab06283007
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-04-18 01:53:24 +0300

    Android Share done

--- app/src/main/java/com/example/robandwiki/MyAdapter.kt
+++ app/src/main/java/com/example/robandwiki/MyAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.robandwiki
 
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
@@ -14,11 +16,23 @@ class MyAdapter(private val bandList : ArrayList<Band>) : RecyclerView.Adapter<M
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
         val currentitem = bandList[position]
+
         holder.bandName.text = currentitem.bandName
         holder.bandLocation.text = currentitem.bandLocation
         holder.bandGenre.text = currentitem.bandGenre
+
+        val btnShare = holder.itemView.findViewById<ImageButton>(R.id.btnShare)
+
+        btnShare.setOnClickListener {
+            val shareIntent = Intent(Intent.ACTION_SEND)
+            shareIntent.type = "text/plain"
+            shareIntent.putExtra(Intent.EXTRA_TEXT, "Check out this band: ${currentitem.bandName} - ${currentitem.bandLocation} (${currentitem.bandGenre})")
+            holder.itemView.context.startActivity(Intent.createChooser(shareIntent, "Share via"))
+        }
     }
+
 
     override fun getItemCount(): Int {
         return bandList.size
