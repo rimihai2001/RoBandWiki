6b53878f012e516c26e6b8a765410b6f1959dada
Radu-Ioan Mihai <rimihai2001@gmail.com>
2023-05-05 00:39:56 +0300

    Video player added

--- app/src/main/java/com/example/robandwiki/fragments/RecommendationFragment.kt
+++ app/src/main/java/com/example/robandwiki/fragments/RecommendationFragment.kt
@@ -1,24 +1,19 @@
 package com.example.robandwiki.fragments
 
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.MediaController
+import android.widget.VideoView
+import androidx.fragment.app.Fragment
 import com.example.robandwiki.R
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [RecommendationFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class RecommendationFragment : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -34,20 +29,22 @@ class RecommendationFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recommendation, container, false)
+        val view = inflater.inflate(R.layout.fragment_recommendation, container, false)
+
+        val videoView = view.findViewById<VideoView>(R.id.videoViewRecommendation)
+        val mediaController = MediaController(requireContext())
+        mediaController.setAnchorView(videoView)
+        videoView.setMediaController(mediaController)
+
+        val videoUri = Uri.parse("android.resource://" + requireContext().packageName + "/" + R.raw.rocklamures)
+        videoView.setVideoURI(videoUri)
+        videoView.requestFocus()
+        videoView.start()
+
+        return view
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RecommendationFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             RecommendationFragment().apply {
